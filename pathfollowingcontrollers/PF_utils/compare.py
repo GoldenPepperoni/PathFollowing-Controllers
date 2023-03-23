69cd68816d89344a6e998876300372da6d1016ea
Jim Wong <wongjim8@gmail.com>
2023-03-23 00:37:32 +0000

    Refactor

--- pathfollowingcontrollers/PF_utils/compare.py
+++ pathfollowingcontrollers/PF_utils/compare.py
@@ -1,1 +1,52 @@
-from pathfollowingcontrollers.algorithms.NLGL import LQR_NLGL_8
+from importlib import import_module
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = np.array([])
+itseArray = []
+iseArray = []
+iaeArray = []
+
+# Initialise Plot
+fig, ax = plt.subplots(layout='constrained')
+width = 0.25  # the width of the bars
+multiplier = 0
+
+if __name__ == "__main__":
+
+    # Handle situation where lesser than 2 algorithms were put to compared
+    if len(sys.argv) <= 2:
+        exit("Comparison need more than 2 algorithms!")
+
+    # Run each algorithm script
+    for i in range(1, len(sys.argv)):
+        algoScript = sys.argv[i] # Extract individual script
+        algoPath = algoScript.replace("/", ".")[:-3] # Formatting
+        algoName = algoPath[1:]
+
+        try:
+            # Run the script by importing
+            algoModule = import_module(algoPath, "pathfollowingcontrollers.algorithms")
+            print(f"Finished running {algoName} !!!")
+        except:
+            exit(f"Unable to run {sys.argv[i+1]} !!!")
+        
+        
+        desiredPath, actualPath, ctrlTraces, tArray = algoModule.getPerformanceData()
+
+        # Calculate errors
+        absErr = np.linalg.norm([actualPath])
+        iae = np.sum(absErr)
+        ise = np.sum(absErr**2)
+        itse = np.matmul(absErr, tArray)
+
+        # Plot bar chart comparing errors
+
+
+        offset = width * multiplier
+        rects = ax.bar(i + offset, iae, width, label=algoName)
+        ax.bar_label(rects, padding=3)
+        multiplier += 1
+
+        plt.show()
--- pathfollowingcontrollers/algorithms/CarrotChasing/climb.py
+++ pathfollowingcontrollers/algorithms/CarrotChasing/climb.py
@@ -10,17 +10,16 @@ from pathfollowingcontrollers.PF_utils.abstractions import *
 
 # r = readDS4() # For PS4 controller live inputs
 
-# NLGL algorithm parameters
-Kphi = 1
+# Carrot chasing algorithm parameters
+Kpsi = 2.5
 Ktheta = 0.5
-L1 = 15
 
-# Create custom figure 8 path
-custom_targets = [[0, 50, 10], [-100, 50, 10], [-200, 50, 10], [-100, 50, 10], [0, 50, 10], [-100, 50, 10], [-200, 50, 10], [-100, 50, 10], [0, 50, 10]]
-custom_yaw_targets = [np.pi/2, -np.pi/2, np.pi/2, -np.pi/2, np.pi/2, -np.pi/2, np.pi/2, -np.pi/2, np.pi/2,] # (-pi to pi)
+# Create custom straight path
+custom_targets = [[100, -100, 30]]
+custom_yaw_targets = [np.pi/2] # (-pi to pi)
 
 # Create and initialise dubins path env
-envs = gymnasium.make("PyFlyt/Fixedwing-NLGLDubinsPath-v0", render_mode=None, angle_representation="euler", spawn_pos=np.array([[10.0, 10.0, 10.0]]), flight_dome_size=500, turning_radius=50, num_targets=len(custom_yaw_targets), custom_targets=custom_targets, custom_yaw_targets=custom_yaw_targets, NLGL_L1=L1)
+envs = gymnasium.make("PyFlyt/Fixedwing-CCDubinsPath-v0", render_mode=None, angle_representation="euler", flight_dome_size=500, turning_radius=50, num_targets=len(custom_yaw_targets), custom_targets=custom_targets, custom_yaw_targets=custom_yaw_targets)
 next_obs, infos =envs.reset(aviary_options={"cameraTargetPosition":[-10, -10, 30]})    
 terminated  = False
 truncated = False
@@ -45,8 +44,8 @@ while not (terminated or truncated):
     s_lat = [[obs[6]], [obs[1]], [obs[2]], [obs[4]]] # [v, p, r, phi]
     s_long = [[obs[7]], [obs[8]], [obs[0]], [obs[3]]] # [u, w, q, theta]
 
-    # Get references from NLGL algorithm (carrot_pos, UAV_pos, UAV_ang, UAV_vel, Kphi, Ktheta, L1)
-    ref_lat, ref_long = getNLGLRefs(carrot_pos, obs[9:12], obs[3:6], obs[6:9], Kphi, Ktheta, L1, 1, 1.4)
+    # Get references from carrot chasing algorithm (carrot_pos, UAV_pos, UAV_ang, UAV_vel, Kpsi, Ktheta)
+    ref_lat, ref_long = getCCRefs(carrot_pos, obs[9:12], obs[3:6], obs[6:9], Kpsi, Ktheta, 1, 1.4)
 
     ref_lat = [ref_lat, 0] # phi and r
     ref_long = [ref_long] # theta
@@ -79,22 +78,17 @@ while not (terminated or truncated):
 
 if makeGif:
     imgs = [Image.fromarray(img) for img in imgs_array]
-    imgs[0].save("LQR_NLGL_8.gif", save_all=True, append_images=imgs[1:], duration=100/3, loop=0)
+    imgs[0].save("LQR_CC_straight.gif", save_all=True, append_images=imgs[1:], duration=100/3, loop=0)
 
 if makePlots:
-    plotXY(desiredPath, actualPath, "Horizontal trajectory (LQR_NLGL_8)")
-    plotZ(desiredPath, actualPath, "Vertical trajectory (LQR_NLGL_8)")
-    plot3D(desiredPath, actualPath, "3D trajectory (LQR_NLGL_8)")
-    plotCtrlTraces(ctrlTraces, tArray, "Control traces (LQR_NLGL_8)")
+    plotXY(desiredPath, actualPath, "Horizontal trajectory (LQR_CC_straight)")
+    plotZ(desiredPath, actualPath, "Vertical trajectory (LQR_CC_straight)")
+    plot3D(desiredPath, actualPath, "3D trajectory (LQR_CC_straight)")
+    plotCtrlTraces(ctrlTraces, tArray, "Control traces (LQR_CC_straight)")
     plt.show()
 
-
-
-if __name__ == "__main__":
-
-    # return desiredPath, actualPath, ctrlTraces, tArray
-
-
+def getPerformanceData():
+    return desiredPath, actualPath, ctrlTraces, tArray
 
 
         
